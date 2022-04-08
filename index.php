c8b032d3572bd053e3c67d73c0079599da19fca4
GiovanniCalia <johnca@outlook.it>
2022-04-08 15:25:59 +0200

    Procedimento secondo snack

--- index.php
+++ index.php
@@ -8,11 +8,13 @@
 </head>
 <body>
 
-<!-- Snack 1
+<!--Snack 1
 Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
-Olimpia Milano - Cantù | 55-60 -->
+Olimpia Milano - Cantù | 55-60-->
 
+<h1>Primo snack</h1>
 <ul><?php
+  
   $arr_partite = [
     [
       "casa" => [
@@ -37,5 +39,31 @@ Olimpia Milano - Cantù | 55-60 -->
   </ul><?php 
   } ?>
   </ul>
+
+<!--Snack 2
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”-->
+<h1>Secondo Snack</h1>
+<?php 
+$name = $_GET["name"];
+$mail = $_GET["mail"];
+$age = $_GET["age"];
+
+
+if (strlen($name) > 3) {
+  echo "tutto ok";
+} else {
+  echo "non va bene";
+}
+?>
+
+<!--Snack 4
+Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta-->
+
+
+
+<!--Snack 5
+Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.-->
+
+
 </body>
 </html>
